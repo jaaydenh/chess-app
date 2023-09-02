34813857f36ec4462605fb365fe1225c3c1cada3
Jaayden Halko <jaayden.halko@gmail.com>
2023-09-02 13:11:31 +0300

    fix: update lint and ts configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -15,4 +17,10 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+};

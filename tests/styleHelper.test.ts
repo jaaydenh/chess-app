464c41ec82fba4e3d7f026d3053aa807d0c004e1
Jaayden Halko <jaayden.halko@gmail.com>
2023-09-03 13:59:15 +0300

    feat: add more tests for stylehelpers

--- tests/styleHelper.test.ts
+++ tests/styleHelper.test.ts
@@ -1,20 +1,20 @@
 import { Chess, Square } from 'chess.js';
 import { describe, it, expect } from "vitest";
-import { showHintsForSquare } from "../src/utils/styleHelpers"
+import { showHintsForSquare, lastMoveStyles, buildSquareStyles } from "../src/utils/styleHelpers"
 
-describe('styleHelpers', () => {
+describe('styleHelpers showHintsForSquare', () => {
   it('should return hints for square', () => {
     const game = new Chess();
     const hints = showHintsForSquare('f2', game)
-    const testData = [{ square: 'f3', capture: false }, { square: 'f4', capture: false }];
-    expect(hints).toStrictEqual(testData);
+    const expected = [{ square: 'f3', capture: false }, { square: 'f4', capture: false }];
+    expect(hints).toStrictEqual(expected);
   })
 
   it('should return hints for square with possible capture', () => {
     const game = new Chess('rnbqkbnr/ppppp1pp/8/5p2/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2');
     const hints = showHintsForSquare('e4', game)
-    const testData = [{ square: 'e5', capture: false }, { square: 'f5', capture: true }];
-    expect(hints).toStrictEqual(testData);
+    const expected = [{ square: 'e5', capture: false }, { square: 'f5', capture: true }];
+    expect(hints).toStrictEqual(expected);
   })
 
   it('should not return hints if move not possible', () => {
@@ -30,4 +30,34 @@ describe('styleHelpers', () => {
   })
 })
 
+describe('styleHelpers lastMoveStyles', () => {
+  it('should return lastMoveStyles', () => {
+    const game = new Chess();
+    game.move({
+      from: 'e2',
+      to: 'e4',
+      promotion: 'q',
+    });
+    const lastMove = lastMoveStyles(game.history({ verbose: true }))
+    console.log(lastMove)
+    const expected = {
+      e2: { backgroundColor: 'hsla(81, 58%, 50%, .6)' },
+      e4: { backgroundColor: 'hsla(81, 58%, 50%, .6)' }
+    };
+    expect(lastMove).toStrictEqual(expected);
+  })
 
+  it('should not return lastMoveStyles with no history', () => {
+    const game = new Chess();
+    const lastMove = lastMoveStyles(game.history({ verbose: true }))
+    expect(lastMove).toStrictEqual(null);
+  })
+})
+
+describe('styleHelpers buildSquareStyles', () => {
+  it('should return an empty object for a new game', () => {
+    const game = new Chess();
+    const lastMove = buildSquareStyles(null, game)
+    expect(lastMove).toStrictEqual({});
+  })
+})
